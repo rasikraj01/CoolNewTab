c7d58ba23d647fb517989a4f0166899af03c9e96
Rasik <rasikraj01@gmail.com>
2017-10-25 23:59:26 +0530

    news added

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,10 +1,12 @@
 var url = ' https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=da63afd1f6814247aeb5a840097824e4';
 
-fetch(url)
-  .then((resp) => resp.json()) // Transform the data into json
-  .then(function(data) {
-    		console.log(data[2])
-  	
-    document.getElementById('news').innerHTML = `${data}`;
-    });
-  
+axios.get(url)
+  .then(function (res) {
+  	for (var i = 0; i < res.data.articles.length; i++) {
+  	document.getElementById('news').innerHTML += `<li><a href="${res.data.articles[i].url}">${res.data.articles[i].title} : ${res.data.articles[i].author} </a> <br /> ${res.data.articles[i].description}</li>`;
+  	}
+    console.log(res.data.articles);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
